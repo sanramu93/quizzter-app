508d875fb586f07bcfeb63801f6431cbfaebcb63
Santiago <sanramu93@gmail.com>
2022-04-04 13:00:27 -0500

    Add spinner loader

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Question from "./Question";
+import Spinner from "./Spinner";
 
 export default function Quiz({ questions, setShowQuiz }) {
   const [isCorrect, setIsCorrect] = useState(false);
@@ -28,17 +29,52 @@ export default function Quiz({ questions, setShowQuiz }) {
   };
 
   const renderQuestions = () => {
-    return questions.map((q, i) => (
-      <Question
-        question={q}
-        index={i}
-        setCurrentQuestion={setCurrentQuestion}
-        setIsCorrect={setIsCorrect}
-        showScore={showScore}
-        questionDisabled={questionDisabled}
-        key={q.question}
-      />
-    ));
+    if (questions.length < 1) return <Spinner />;
+    else {
+      return questions.map((q, i) => (
+        <Question
+          question={q}
+          index={i}
+          setCurrentQuestion={setCurrentQuestion}
+          setIsCorrect={setIsCorrect}
+          showScore={showScore}
+          questionDisabled={questionDisabled}
+          key={q.question}
+        />
+      ));
+    }
+  };
+
+  const renderBottom = () => {
+    if (questions.length < 1) return;
+    else {
+      return !showScore ? (
+        <button
+          className="btn btn--check-answers "
+          onClick={handleCheckAnswers}
+        >
+          Check Answers
+        </button>
+      ) : (
+        <>
+          <div className="quiz__score">
+            <h2 className="score__message">
+              You scored <span className="score__num">{score}</span>
+              <span className="score__num-slash">/</span>
+              <span className="score__num">{questions.length}</span> correct
+              answers
+            </h2>
+          </div>
+          <Link
+            to="/"
+            className="btn btn--play-again"
+            onClick={handlePlayAgain}
+          >
+            Play Again
+          </Link>
+        </>
+      );
+    }
   };
 
   useEffect(() => {
@@ -55,34 +91,7 @@ export default function Quiz({ questions, setShowQuiz }) {
   return (
     <section className="quiz">
       {renderQuestions()}
-      <div className="quiz__bottom">
-        {!showScore ? (
-          <button
-            className="btn btn--check-answers "
-            onClick={handleCheckAnswers}
-          >
-            Check Answers
-          </button>
-        ) : (
-          <>
-            <div className="quiz__score">
-              <h2 className="score__message">
-                You scored <span className="score__num">{score}</span>
-                <span className="score__num-slash">/</span>
-                <span className="score__num">{questions.length}</span> correct
-                answers
-              </h2>
-            </div>
-            <Link
-              to="/"
-              className="btn btn--play-again"
-              onClick={handlePlayAgain}
-            >
-              Play Again
-            </Link>
-          </>
-        )}
-      </div>
+      <div className="quiz__bottom">{renderBottom()}</div>
     </section>
   );
 }
---
+++ src/components/Spinner.js
@@ -0,0 +1,8 @@
+export default function Spinner() {
+  return (
+    <div className="loader-wrapper">
+      <div className="loader"></div>
+      <h2 className="loader__text">Loading...</h2>
+    </div>
+  );
+}
